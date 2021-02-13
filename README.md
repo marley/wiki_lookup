9e52f580d847cb263cf6c781a9d19785ae5c9552
Marley Alford <marleycalford@gmail.com>
2021-02-13 02:02:40 -0500

    Popup now shows highlighted text and wiki search result link or error

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Summary
 
-# To Run
+This chrome extension allows you to see if a wikipedia article exists on any word or phrase you highlight.
 
-# Notes
+# To Use
 
-The first draft of this extension I made with the help of https://medium.com/tech-tajawal/build-a-simple-google-chrome-extension-in-few-minutes-1f13b600e83e
+1.  Download Wiki Lookup from Chrome Store
+2.  Enable and pin extension
+3.  Highlight a word on Chrome web browser
+4.  Click Wiki Lookup icon
+5.  If page exists, you will see a link to the wikipedia article in the Wiki Lookup popup. If not, you will see an error message.
+
+# Helpful Links
+
+- The first draft of this extension I made with the help of https://medium.com/tech-tajawal/build-a-simple-google-chrome-extension-in-few-minutes-1f13b600e83e
+- How to safely open a new tab https://www.freecodecamp.org/news/how-to-use-html-to-open-link-in-new-tab/
+
+# Next Steps
+
+- [ ] Add button to create new wikipedia article if it does not exist
--- popup.js
+++ popup.js
@@ -3,8 +3,64 @@ chrome.tabs.executeScript(
     code: "window.getSelection().toString();",
   },
   function (selection) {
-    var output = document.getElementById("output");
-    output.innerHTML = selection[0];
-    // alert(selection[0]);
+    var textSelection = document.getElementById("textSelection");
+    let searchPhrase = selection[0];
+    textSelection.innerHTML = searchPhrase;
+
+    // checkPageButton.addEventListener(
+    //   "click",
+    //   (searchPhrase) => searchWikipedia(searchPhrase),
+    //   false
+    // );
+    fetch(
+      `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchPhrase}&limit=1&namespace=0&format=json&origin=*`
+    )
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function (data) {
+          console.log(data[3].length);
+          if (data[3].length > 0) {
+            var result = document.getElementById("searchResult");
+            // result.innerHTML = `<a href=${data[3]}></a>`;
+            result.innerHTML = `<a href="${data[3]}" target="_blank" rel="noopener noreferrer">${data[1]}</a>`;
+          } else {
+            var result = document.getElementById("searchError");
+            result.innerHTML = "No wikipedia article found!";
+          }
+        });
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
   }
 );
+
+async function searchWikipedia(searchPhrase) {
+  console.log(`searching for ${searchPhrase}`);
+  fetch(
+    `https://en.wikipedia.org/w/api.php?action=query&titles=${searchPhrase}&prop=images&format=json&origin=*`
+  )
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.log(
+          "Looks like there was a problem. Status Code: " + response.status
+        );
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    })
+    .catch(function (err) {
+      console.log("Fetch Error :-S", err);
+    });
+}
